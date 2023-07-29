c210683ede188421f754e39bc576f2146b62eafa
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-29 15:06:47 +0400

    FlightTableViewCell added

---
+++ AirportTimetable/Views/FlightTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  FlightTableViewCell.swift
+//  AirportTimetable
+//
+//  Created by Vsevolod Lashin on 29.07.2023.
+//
+
+import UIKit
+
+final class FlightTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
