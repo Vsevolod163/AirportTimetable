94ee369bdd9ec39e073ebced0795574bfa7bd26a
Vsevolod <vsevolod.lashin@gmail.com>
2023-07-29 15:05:09 +0400

    DepartureVC added

---
+++ AirportTimetable/ViewControllers/DepartureViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DepartureViewController.swift
+//  AirportTimetable
+//
+//  Created by Vsevolod Lashin on 29.07.2023.
+//
+
+import UIKit
+
+final class DepartureViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
